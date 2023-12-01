b17493c1493e8a5c9321f7a32c7996f65ec46a32
enbeze <150516965+enbeze@users.noreply.github.com>
2023-12-01 09:14:52 +0100

    Update main.tf, added file content template and create file_count number of files

--- main.tf
+++ main.tf
@@ -6,7 +6,15 @@ variable "file_name" {
   type = string
   default = "teszt001"
 }
+locals {
+  file_content_template = "This is file number %d."
+}
 resource "local_file" "first_file" {
   content = "AtomAntik"
   filename = "../first_file.txt"
+}
+resource "local_file" "count_file" {
+  count = var.file_count
+  content    = format(local.file_content_template, count.index + 1)
+  filename   = "file_${count.index + 1}.txt"
 }
