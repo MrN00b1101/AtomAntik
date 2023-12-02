fe68e65740297fffdb9ae60c92633c3ee5052177
enbeze <150516965+enbeze@users.noreply.github.com>
2023-12-02 09:07:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,24 +4,24 @@ variable "file_count" {
 }
 variable "file_name" {
   type = string
-  default = "teszt001"
+  default = "./teszt001"
 }
 locals {
   file_content_template = "This is file number %d."
 }
 resource "local_file" "first_file_1" {
   content = "AtomAntik"
-  filename = "first_file_1.txt"
+  filename = "./first_file_1.txt"
 }
 resource "local_file" "count_file_1" {
   count = var.file_count
   content    = format(local.file_content_template, count.index + 1)
-  filename   = "file_${count.index + 1}.txt"
+  filename   = "./file_${count.index + 1}.txt"
 }
 
 resource "local_file" "first_file" {
   content = "This is my text"
-  filename = "first_file.txt"
+  filename = "./first_file.txt"
 }
 
 resource "local_file" "variable_file" {
