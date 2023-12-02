024aa524e070e4bf0baa20ccdc70e7f6fb04f698
Balázs Varga <34116704+MrN00b1101@users.noreply.github.com>
2023-12-02 09:16:02 +0100

    fixing the file folders

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     # Check if specific files exist teszt001 first_file.txt
-    if   [ -f "file_3.txt" ] && [ -f "file_2.txt" ] && [ -f "file_1.txt" ] && [ -f "first_file.txt" ] && [ -f "teszt001.txt" ]; then
+    if   [ -f "./file_3.txt" ] && [ -f "./file_2.txt" ] && [ -f "./file_1.txt" ] && [ -f "./first_file.txt" ] && [ -f "./teszt001.txt" ]; then
         echo "Validation successful. All required files exist."
         exit 0  # Exit with success status
     else
